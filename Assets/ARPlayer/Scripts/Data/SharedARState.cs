94fbd2029c5bcd4b2a033fa08fb8e75626d01265
HYPER Kao <HYPER.Kao@gmail.com>
2022-02-08 11:29:22 +0800

    fix: Unity.Plastic.Netwonsoft.Json is EditorOnly package...

--- Assets/ARPlayer/Scripts/Data/SharedARState.cs
+++ Assets/ARPlayer/Scripts/Data/SharedARState.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Plastic.Newtonsoft.Json;
+using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
